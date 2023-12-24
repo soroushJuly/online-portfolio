603a7c48508675b6f7102611299885bbf4f514ca
soroushJuly <soroushjuly@gmail.com>
2023-12-24 13:44:52 +0000

    add: data util file

---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+export const langs = [
+  {
+    id: 0,
+    name: "C/C++",
+    description:
+      "Description of the level and experince and things I have done",
+  },
+
+  { id: 1, name: "JavaScripts", description: "ffd" },
+  { id: 2, name: "Python", description: "ddd" },
+];
+
+export const tools = [
+  {
+    id: 0,
+    name: "Unreal Engine",
+    description:
+      "Description of the level and experince and things I have done",
+  },
+  { id: 1, name: "Unity Engine", description: "ffd" },
+  { id: 2, name: "OpenGL", description: "ddd" },
+  { id: 3, name: "Vue & React", description: "ddd" },
+  { id: 4, name: "Git", description: "ddd" },
+];
+
+export const about = {
+  p1: "Hi, it’s Soroush!",
+  p2: "I’m a software engineer eager about game development. I worked as a software engineer in different teams in the tech industry for about 2 years. I’m currently studying for a master’s in computer games technology with an expected end date of August 2024. The focus of my course is on C++ programming and learning the essential math skills to build high-quality games, I’ve also completed an in-depth Udemy course on C++ and Unreal engine, therefore I have a robust understanding of C++. Based on my experience I can learn new programming languages fast and can easily understand and work on top of others’ codes. Also, I’m comfortable using company-specific engines to build a game as I did in my course. On top of that, I’m working on the game engine  we used at the university and improving it. I have had a passion  for games from a young age and Now I can’t wait to face new challenges in game programming.",
+  p3: "",
+};
+
+export const interests = [];
