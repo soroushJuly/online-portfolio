a7b041302774b20d58ae66b8d831e4f9ca8c8190
soroushJuly <soroushjuly@gmail.com>
2023-12-24 13:44:38 +0000

    chore: skill section

--- src/components/home/skills.js
+++ src/components/home/skills.js
@@ -1,29 +1,43 @@
 "use client";
 
 import { useState } from "react";
+import { langs, tools } from "@/utils/data";
 
 import "./skills.css";
 
 const Skills = () => {
   const [activeLang, setLang] = useState(0);
+  const [activeTool, setTool] = useState(0);
 
-  const langs = [
-    {
-      name: "C/C++",
-      description:
-        "Description of the level and experince and things I have done",
-    },
-    { name: "JavaScripts", description: "ffd" },
-    { name: "Python", description: "ddd" },
-  ];
+  const langListItems = langs.map((lang) => (
+    <li key={lang.id}>
+      <button
+        className={lang.id == activeLang ? "active" : ""}
+        onClick={() => handleLangClick(lang.id)}
+      >
+        {lang.name}
+      </button>
+    </li>
+  ));
 
-  //   cons;
+  const toolListItems = tools.map((tool) => (
+    <li key={tool.id}>
+      <button
+        className={tool.id == activeTool ? "active" : ""}
+        onClick={() => handleToolClick(tool.id)}
+      >
+        {tool.name}
+      </button>
+    </li>
+  ));
 
   const handleLangClick = (index) => {
     setLang(index);
   };
 
-  //   function handleToolClick(params) {}
+  const handleToolClick = (index) => {
+    setTool(index);
+  };
 
   return (
     <section id="skills" className="Section Section--white">
@@ -33,46 +47,21 @@ const Skills = () => {
       </p>
       <h4 className=" text-3xl underline my-4">Programming languages:</h4>
       <ul className=" Button-list flex text-xl font-semibold mb-2">
-        <li>
-          <button onClick={() => handleLangClick(0)}>{langs[0].name}</button>
-        </li>
-        <li>
-          <button onClick={() => handleLangClick(1)}>{langs[1].name}</button>
-        </li>
-        <li>
-          <button onClick={() => handleLangClick(2)}>{langs[2].name}</button>
-        </li>
+        {langListItems}
       </ul>
-
-      <p className=" border-2 rounded border-gray-400 p-2">
+      <p className=" border-2 rounded border-gray-400 p-3">
         {langs[activeLang].description}
       </p>
 
       <h4 className=" text-3xl underline my-4">
-        Engines\Frameworks\Software Used Frequently:
+        Engines\Frameworks\Software Used Most Frequently:
       </h4>
-      <ul>
-        <li>
-          <button>Unreal Engine</button>
-        </li>
-        <li>
-          <button>Unity Engine</button>
-        </li>
-        <li>
-          <button>OpenGL</button>
-        </li>
-        <li>
-          <button>Vue & React</button>
-        </li>
-        <li>
-          <button>Git</button>
-        </li>
-        <li>
-          <button>Git</button>
-        </li>
+      <ul className=" Button-list flex text-xl font-semibold mb-2">
+        {toolListItems}
       </ul>
-
-      <p>Description of the level and experince and things I have done</p>
+      <p className=" border-2 rounded border-gray-400 p-3 mb-4">
+        {tools[activeTool].description}
+      </p>
     </section>
   );
 };
