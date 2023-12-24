5c078f57ce11c78592469b7174863ba20ddd00ad
soroushJuly <soroushjuly@gmail.com>
2023-12-24 13:45:09 +0000

    chore: about section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import SectionHead from "@/components/home/head";
 import SectionContactMe from "@/components/home/contact";
 import SectionSkills from "@/components/home/skills";
 
+import { about } from "@/utils/data";
+
 import "./home.css";
 
 export default function Home() {
@@ -13,37 +15,49 @@ export default function Home() {
         <h3 className="Title">About me</h3>
         <div className=" grid grid-cols-2 justify-center">
           <div className=" text-justify">
-            <p>Hi, it’s Soroush</p>
-            <p>
-              I’m a software engineer eager about game development. I worked as
-              a software engineer in different teams in the tech industry for
-              about 2 years. I’m currently studying for a master’s in computer
-              games technology with an expected end date of August 2024. The
-              focus of my course is on C++ programming and learning the
-              essential math skills to build high-quality games, I’ve also
-              completed an in-depth Udemy course on C++ and Unreal engine,
-              therefore I have a robust understanding of C++. Based on my
-              experience I can learn new programming languages fast and can
-              easily understand and work on top of others’ codes. Also, I’m
-              comfortable using company-specific engines to build a game as I
-              did in my course. On top of that, I’m working on the game engine
-              we used at the university and improving it. I have had a passion
-              for games from a young age and Now I can’t wait to face new
-              challenges in game programming.
-            </p>
-            <p>
+            <p className=" text-lg mb-2">{about.p1}</p>
+            <p className=" text-lg mb-2">{about.p2}</p>
+            <p className=" text-lg mb-2">
               I’m always open to talk and I like to collaborate on different
-              projects.Feel free to reach me by email. Our links below
+              projects.Feel free to reach me by{" "}
+              <a className=" text-blue-500 underline" href="#contact">
+                email
+              </a>
+              . Our links below
             </p>
-            <ul>Linkedin link, github</ul>
+            <ul className="flex">
+              <li className="mr-3 hover:bg-orange-200">
+                <a
+                  href="http://www.linkedin.com/in/soroush-july-jolai"
+                  target="blank"
+                >
+                  <Image
+                    alt="LinkedIn Icon"
+                    width="30"
+                    height="30"
+                    src="/linkedin.svg"
+                  />
+                </a>
+              </li>
+              <li className="hover:bg-orange-200 rounded-full">
+                <a href="https://github.com/soroushJuly" target="blank">
+                  <Image
+                    alt="Github Icon"
+                    width="30"
+                    height="30"
+                    src="/github.svg"
+                  />
+                </a>
+              </li>
+            </ul>
           </div>
-          <div>
-            <h4>Interests</h4>
-            <ul className="flex flex-col">
-              <li>interest 1</li>
-              <li>interest 2</li>
-              <li>interest 3</li>
-              <li>interest 4</li>
+          <div className="md:ml-8">
+            <h4 className=" text-xl font-semibold mb-4">My Interests:</h4>
+            <ul className="flex flex-col text-lg pl-6">
+              <li className=" list-disc list-inside">interest 1</li>
+              <li className="list-disc list-inside">interest 2</li>
+              <li className="list-disc list-inside">interest 3</li>
+              <li className="list-disc list-inside">interest 4</li>
             </ul>
           </div>
         </div>
