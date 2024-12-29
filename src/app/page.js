c87cc26e8ee765c61d1626e8af1d50b17dc24d97
Soroush <soroushdevelope@gmail.com>
2024-12-29 00:34:34 +0000

    remove the about section from main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import SectionHead from "@/components/home/head";
 import SectionContactMe from "@/components/home/contact";
 import SectionSkills from "@/components/home/skills";
-import SectionAbout from "@/components/home/about";
 import SectionPortfolio from "@/components/home/portfolio";
 
 import "./home.css";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <SectionHead />
-      <SectionAbout />
       <SectionPortfolio />
       <SectionSkills />
       <SectionContactMe />
