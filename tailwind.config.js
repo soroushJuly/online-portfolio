c71606c832cb462c23b338bcd507e1aadd99a6e2
Soroush <soroushdevelope@gmail.com>
2024-12-29 00:33:05 +0000

    feat: update next js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "colorBg": "#FAF9F6",
+        "colorBgAccent": "#F4F1ED",
+        "colorCardPrimary": "#EAF6F6",
+        "colorCardSecondary": "#FEE4E1",
+        "colorLinkHover": "#FFC857",
+        "colorSecondary": "#8A8B49",
+        "colorTertiary": "#f26c62",
+        "text-color": "#333333",
+        "warning": "#FFD700",
+        "success": "#50C878",
+        "error": "#DC143C",
+        "active": "#4169E1",
+      }
     },
   },
   plugins: [],
